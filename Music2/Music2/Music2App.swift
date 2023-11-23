1e9b1a25d6f0b51f66d0b47de9fea6dd46d0e7c4
DanielePerrupane <148336607+DanielePerrupane@users.noreply.github.com>
2023-11-23 21:29:34 +0100

    first commit

---
+++ Music2/Music2/Music2App.swift
@@ -0,0 +1,17 @@
+//
+//  Music2App.swift
+//  Music2
+//
+//  Created by Daniele Perrupane on 23/11/23.
+//
+
+import SwiftUI
+
+@main
+struct Music2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
